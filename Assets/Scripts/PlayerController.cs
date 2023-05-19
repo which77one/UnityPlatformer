74bc47356b6837ddf37a9635c23b7bbdd0d57049
which77one <thienanltv1207@gmail.com>
2023-05-19 10:53:46 +0700

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     [SerializeField] private Rigidbody2D rb;
+     [SerializeField] private Transform groundCheck;
+     [SerializeField] private LayerMask groundLayer;
+     
+
+    private float x_axis;
+    public float movementSpeed= 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    void Update()
+    {
+        x_axis = Input.GetAxisRaw("Horizontal");
+
+        Jump();
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+       rb.velocity  = new Vector2(x_axis * movementSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && x_axis <0f || !isFacingRight && x_axis> 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+   private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+}
