a9dbd881f1940c0ef3623c5df6c0d72d36e9f18d
which77one <130749158+which77one@users.noreply.github.com>
2023-05-22 14:55:15 +0700

    FEADGGSAFGFS

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,80 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-     [SerializeField] private Rigidbody2D rb;
-     [SerializeField] private Transform groundCheck;
-     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
+
 
     public Animator animator;
+    
 
-
+    //Walk
     private float x_axis;
-    public float movementSpeed= 8f;
+    public float movementSpeed = 8f;
     public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+
+    //Jumping with force
+    public float jumpHeight;
+    public float runMaxSpeed; //Target speed we want the player to reach.
+    public float runAcceleration; //Time (approx.) time we want it to take for the player to accelerate from 0 to the runMaxSpeed.
+    [HideInInspector] public float runAccelAmount; //The actual force (multiplied with speedDiff) applied to the player.
+    public float runDecceleration; //Time (approx.) we want it to take for the player to accelerate from runMaxSpeed to 0
+    [HideInInspector] public float runDeccelAmount; //Actual force (multiplied with speedDiff) applied to the player .
+    [Range(0.01f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
+    [Range(0.01f, 1)] public float deccelInAir;
+    public bool doConserveMomentum;
+    public float LastOnGroundTime;
+
+    //Dash
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 10f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    void Start()
+    {
+
+    }
     void Update()
     {
         x_axis = Input.GetAxisRaw("Horizontal");
+        LastOnGroundTime -= Time.deltaTime;
+        if (isDashing)
+        {
+            return;
+        }
         animator.SetFloat("Speed", Mathf.Abs(x_axis));
         Jump();
         Flip();
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+       
+        
+        if (IsGrounded())
+        {
+            LastOnGroundTime = 0.1f;
+        }
     }
 
     private void FixedUpdate()
     {
-       rb.velocity  = new Vector2(x_axis * movementSpeed, rb.velocity.y);
-   
+        if (isDashing)
+        {
+            return;
+        }
+        rb.velocity = new Vector2(x_axis * movementSpeed, rb.velocity.y);
+        JumpingMomentum();
+
     }
 
     private bool IsGrounded()
@@ -37,7 +84,7 @@ public class PlayerController : MonoBehaviour
 
     private void Flip()
     {
-        if (isFacingRight && x_axis <0f || !isFacingRight && x_axis> 0f)
+        if (isFacingRight && x_axis < 0f || !isFacingRight && x_axis > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
@@ -45,7 +92,7 @@ public class PlayerController : MonoBehaviour
             transform.localScale = localScale;
         }
     }
-   private void Jump()
+    private void Jump()
     {
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -54,9 +101,51 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpHeight);
         }
     }
-    
-    
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+    private void OnValidate()
+    {
+        //Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
+        runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
+        runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+            
+        runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+        runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+        
+    }
+    private void JumpingMomentum()
+    {
+        float targetSpeed = x_axis * runMaxSpeed;
+        float accelRate;
+        if (LastOnGroundTime > 0)
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount : runDeccelAmount;
+        else
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount * accelInAir : runDeccelAmount * deccelInAir;
+        if (doConserveMomentum && Mathf.Abs(rb.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(rb.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
+        {
+            //Prevent any deceleration from happening, or in other words conserve are current momentum
+
+            accelRate = 0;
+        }
+        float speedDif = targetSpeed - rb.velocity.x;
+        float movement = speedDif * accelRate;
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
+    }
 }
