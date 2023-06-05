55bc2d8fd8f141d3cde2a3eaf162a803d1ee458c
which77one <130749158+which77one@users.noreply.github.com>
2023-06-05 14:52:27 +0700

    fadf

--- Assets/Scripts/DisapearingPlatform.cs
+++ Assets/Scripts/DisapearingPlatform.cs
@@ -1,17 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DisapearingPlatform : MonoBehaviour
 {
-    //public Animator anim;
+    public Animator anim;
+    public bool HasBeenDestroyed;
     void Awake()
     {
-       //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        //col.GetComponent<BoxCollider>().enabled = false;
-        gameObject.SetActive(false);
+
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(DisappearTime());
+            if (HasBeenDestroyed = true)
+            {
+                StartCoroutine(ReappearTime());
+            }
+        }
+    }
+
+
+    IEnumerator DisappearTime()
+    {
+        anim.SetTrigger("IsDisappearing");
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        HasBeenDestroyed = true;
+    }
+    IEnumerator ReappearTime()
+    {
+        anim.SetTrigger("IsReappearing");
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
     }
 }
